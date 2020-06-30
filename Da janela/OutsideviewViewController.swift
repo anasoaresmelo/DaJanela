7cc55579a6893861f044bdb9844f159190334f42
Ana Melo <anacsoaremelo@gmail.com>
2020-06-30 17:55:12 -0300

    Adiciona autolayout na tela-predio e atualiza Assets

--- Da janela/OutsideviewViewController.swift
+++ Da janela/OutsideviewViewController.swift
@@ -9,13 +9,51 @@
 import UIKit
 
 class OutsideviewViewController: UIViewController {
-
+    
+    @IBOutlet weak var outsideImage: UIImageView!
+    @IBOutlet weak var neighborButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //MARK: Autolayout
+        //Autolayout for Outside view Image
+        outsideImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            outsideImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            outsideImage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            outsideImage.widthAnchor.constraint(equalToConstant: self.view.frame.width*1.05)
+        ])
+        
+        //Autolayout for Neighbor Window Button
+        neighborButton.backgroundColor = .clear
+        neighborButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            neighborButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 70),
+            neighborButton.topAnchor.constraint(equalTo: self.view.centerYAnchor),
+            neighborButton.heightAnchor.constraint(equalToConstant: self.view.frame.height*0.20),
+            neighborButton.widthAnchor.constraint(equalToConstant: self.view.frame.width*0.30)
+        ])
+        
+        //Autolayout for Neighbor Window Button
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 70)
+        ])
+        
+        //MARK: Buttons Targets
+        backButton.addTarget(self, action: #selector(OutsideviewViewController.touchedBackButton), for: .touchUpInside)
     }
-
-
+    
+    //MARK: Buttons Actions
+    @IBAction func touchedBackButton(){
+           navigationController?.popViewController(animated: true)
+       }
 }
--- Da janela/SettingsViewController.swift
+++ Da janela/SettingsViewController.swift
@@ -9,14 +9,39 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
-    
+    let tableView = UITableView()
+    var safeArea: UILayoutGuide!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        safeArea = view.layoutMarginsGuide
+        
+        setupTableView()
+    }
+    
+    func setupTableView() {
+      view.addSubview(tableView)
+      tableView.translatesAutoresizingMaskIntoConstraints = false
+      tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+      tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+      tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+      tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
-
 }
+var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return characters.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = characters[indexPath.row]
+    return cell
+  }
+}
